41b373a3b0ccd64b6befb8d0d59262f901a0df6a
Olivier <47295195+sympact06@users.noreply.github.com>
2024-04-18 21:02:34 +0200

    Removed redundant test, refactored profile tests
    The commit includes the removal of an unnecessary test file and a simplification of the profile tests. The ExampleTest.php file was deleted as it was not needed. In ProfileTest.php, the 'profile page is displayed' test was removed to streamline testing process.

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -2,15 +2,7 @@
 
 use App\Models\User;
 
-test('profile page is displayed', function () {
-    $user = User::factory()->create();
 
-    $response = $this
-        ->actingAs($user)
-        ->get('/profile');
-
-    $response->assertOk();
-});
 
 test('profile information can be updated', function () {
     $user = User::factory()->create();
